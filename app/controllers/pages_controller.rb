55f4ec35f6afcada4ae0160a43caed67ab8de937
Sam Weerasinghe <oystersauce8@gmail.com>
2016-02-03 19:36:28 -0800

    Fix for the message We're sorry, but something went wrong

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!, only: [:account_home, :edit_profile, :restaurants]
+  before_action :authenticate_user!, only: [:account_home, :edit_profile, :restaurants, :search]
   def landing
     if user_signed_in?
       redirect_to pages_account_home_path
