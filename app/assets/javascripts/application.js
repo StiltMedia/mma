e02329e05b941a6c42616fa6efc0b0fa705415bf
Sam Weerasinghe <oystersauce8@gmail.com>
2016-02-03 18:59:04 -0800

    when search, respond to pressing enter too

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -56,35 +56,26 @@ $(document).on('ready page:load', function () {
     $(this).closest("form").submit();
   });
 
+  // search page overlay
+  $('.footer-nav li.search a, .account-box a.search').on('click', function () {
+      $('.search-popup').addClass('active');
+      $('body').css('overflowY', 'hidden');
+      $('.search-popup input').focus();
+  });
+  $('.search-popup .close-overlay').on('click', function () {
+      $('.search-popup').removeClass('active');
+      $('body').css('overflowY', 'auto');
+  });
+
+  //$(".search-popup img.icon_search").on('click', function() {
+  //  $(this).closest('form').find("input#q").first().val( $(".search-popup input.form-alt.white" ).val() );
+  //  $(this).closest('form').submit();
+  //});
+
+
 });
 
 function show_dialog_saving() {
   $('#saving-3').modal('show');  
 }
 
-
-
-
-$(function() {
-
-
-    // search page overlay
-    $('.footer-nav li.search a, .account-box a.search').on('click', function () {
-        $('.search-popup').addClass('active');
-        $('body').css('overflowY', 'hidden');
-        $('.search-popup input').focus();
-    });
-    $('.search-popup .close-overlay').on('click', function () {
-        $('.search-popup').removeClass('active');
-        $('body').css('overflowY', 'auto');
-    });
-
-    $(".search-popup img.icon_search").on('click', function() {
-      $(this).closest('form').find("input#q").first().val( $(".search-popup input.form-alt.white" ).val() );
-      $(this).closest('form').submit();
-    });
-
-    $(".account-box a.search").on('click', function() {
-
-    });
-});
