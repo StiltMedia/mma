cdca06c1313391cbd33c9c9e513531ae2b50de76
Jeff Thomas <subskriptions@gmail.com>
2016-06-20 11:39:38 -0600

    Add chef role to admin page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -119,6 +119,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :picture, :admin)
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :picture, :admin, :chef)
     end
 end
